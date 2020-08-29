8298d21dff5fe58875cb5a2fe3ad427362b71186
Your Name <you@example.com>
2020-08-29 13:01:16 +0300

    first commit

--- src/utils/covid.js
+++ src/utils/covid.js
@@ -1,24 +1,24 @@
-const request=require('request')
-const covid=(country,callback)=>{
-    const url='https://covid19restapi.herokuapp.com/country?countryname='+country
+const request = require("request");
+const covid = (country, callback) => {
+  const url = "https://api.covid19api.com/total/country?countryname=" + country;
 
-    request({url,json:true},(error,data)=>{
-        if(error){
-            callback('please check your net connection and try again',undefined)
-        }
-        else if(data.body===undefined){
-            callback('you did not enter a country.please choose again',undefined)
-        }
-        else{
-            callback(undefined,{
-                countryName:data.body.countryName,
-                total:data.body.total,
-                death:data.body.death,
-                recovered:data.body.recovered,
-                active:data.body.active
-            })
-        }
-    })
-}
+  request({ url, json: true }, (error, data) => {
+    if (error) {
+      callback("please check your net connection and try again", undefined);
+    } else if (data.body === undefined) {
+      callback("you did not enter a country.please choose again", undefined);
+    } else {
+      const dd = data.body[data.body.length - 1];
 
-module.exports=covid
+      callback(undefined, {
+        countryName: dd.Country,
+        total: dd.Confirmed,
+        death: dd.Deaths,
+        recovered: dd.Recovered,
+        active: dd.Active,
+      });
+    }
+  });
+};
+
+module.exports = covid;
